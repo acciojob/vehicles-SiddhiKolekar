d04c0f3886efa4e34246222e9856d025d34260a8
Siddhi <siddhikolekar2018@gmaiil.com>
2024-11-04 20:58:09 +0530

    code

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -12,6 +12,9 @@ public class Vehicle {
         this.currentDirection = 0;
     }
 
+    public String getName() {
+        return name;
+    }
 
     public int getCurrentSpeed() {
         return currentSpeed;
